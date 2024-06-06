84f5f4eced638b708c9e9f77eedc9e8ca5429724
Chaithya G R <chaithyagr@gmail.com>
2024-06-06 14:26:31 +0200

    Fix

--- src/mrinufft/io/nsp.py
+++ src/mrinufft/io/nsp.py
@@ -251,8 +251,8 @@ def write_trajectory(
 
 def read_trajectory(
     grad_filename: str,
-    dwell_time: float = DEFAULT_RASTER_TIME,
-    num_adc_samples: int|str = None,
+    dwell_time: float|str = DEFAULT_RASTER_TIME,
+    num_adc_samples: int = None,
     gamma: Gammas | float = Gammas.HYDROGEN,
     raster_time: float = DEFAULT_RASTER_TIME,
     read_shots: bool = False,
@@ -264,8 +264,11 @@ def read_trajectory(
     ----------
     grad_filename : str
         Gradient filename.
-    dwell_time : float, optional
+    dwell_time : float|str, optional
         Dwell time of ADC in ms, by default 0.01
+        It can also be string 'min_osf' to select dwell time
+        based on minimum OSF needed to get Nyquist sampling 
+        (This is obtained from SPARKLING trajectory header).
     num_adc_samples : int, optional
         Number of ADC samples, by default None
     gamma : float, optional
@@ -284,8 +287,6 @@ def read_trajectory(
     kspace_loc : np.ndarray
         K-space locations. Shape (num_shots, num_adc_samples, dimension).
     """
-    dwell_time_ns = dwell_time * 1e6
-    gradient_raster_time_ns = raster_time * 1e6
     with open(grad_filename, "rb") as binfile:
         data = np.fromfile(binfile, dtype=np.float32)
         if float(data[0]) > 4:
@@ -300,6 +301,8 @@ def read_trajectory(
             min_osf, data = _pop_elements(data, type="int")
             gamma, data = _pop_elements(data)
             gamma = gamma / 1000
+            if dwell_time == "min_osf":
+                dwell_time = raster_time / min_osf
         (num_shots, num_samples_per_shot), data = _pop_elements(data, 2, type="int")
         if num_adc_samples is None:
             if read_shots:
@@ -335,6 +338,8 @@ def read_trajectory(
         gradients = np.reshape(gradients * 1e-3, (-1, num_samples_per_shot, dimension))
         kspace_loc = np.zeros((num_shots, num_adc_samples, dimension))
         kspace_loc[:, 0, :] = initial_positions
+        dwell_time_ns = dwell_time * 1e6
+        gradient_raster_time_ns = raster_time * 1e6
         adc_times = dwell_time_ns * np.arange(1, num_adc_samples)
         Q, R = divmod(adc_times, gradient_raster_time_ns)
         Q = Q.astype("int")
